1cc8f8ecf53e66e405c2ea5e4590c51c1aa211c7
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-01-22 12:25:07 +0100

    Don't require bson_ext for jRuby.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,15 +4,9 @@ require 'rspec'
 require 'rdf/mongo'
 
 RSpec.configure do |config|
-  #config.include(RDF::Spec::Matchers)
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
   config.exclusion_filter = {
     :ruby           => lambda { |version| RUBY_VERSION.to_s !~ /^#{version}/},
-    :blank_nodes    => 'unique',
-    :arithmetic     => 'native',
-    :sparql_algebra => false,
-    #:status         => 'bug',
-    :reduced        => 'all',
   }
 end
