e129bffe9ca58735c986bb5782bf46d71102c190
Gregg Kellogg <gregg@greggkellogg.net>
2014-05-19 14:46:31 -0700

    Add test for issue #1 (passes).

--- spec/mongo_repository_spec.rb
+++ spec/mongo_repository_spec.rb
@@ -5,19 +5,29 @@ require 'rdf/spec/repository'
 require 'rdf/mongo'
 
 describe RDF::Mongo::Repository do
-  context "Mongo RDF Repository" do
-    before :each do
-      @repository = RDF::Mongo::Repository.new() # TODO: Do you need constructor arguments?
-      @repository.coll.drop
-    end
-   
-    after :each do
-      @repository.coll.drop
-    end
-
-    # @see lib/rdf/spec/repository.rb in RDF-spec
-    include RDF_Repository
+  before :each do
+    @repository = RDF::Mongo::Repository.new() # TODO: Do you need constructor arguments?
+    @repository.coll.drop
+  end
+ 
+  after :each do
+    @repository.coll.drop
   end
 
+  # @see lib/rdf/spec/repository.rb in RDF-spec
+  include RDF_Repository
+
+  context "problemantic examples" do
+    subject {@repository}
+    {
+      "Insert key too large to index" => %(
+        <http://repository.librario.de/publications/0cbdc7f4-728d-4f85-ab09-01060c7b2922> <http://purl.org/ontology/bibo/abstract> "#{'a' * 1001}" .
+      )
+    }.each do |name, nt|
+      it name do
+        expect {subject << RDF::Graph.new << RDF::NTriples::Reader.new(nt)}.not_to raise_error
+      end
+    end
+  end
 end
 
